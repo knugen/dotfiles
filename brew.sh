c7c9b07a6b8f1bc6ea87b17b497b0103b22517c2
Erik Johansson <erik.pm.johansson@gmail.com>
2017-07-30 18:28:27 +0300

    Adding my own stuff

--- brew.sh
+++ brew.sh
@@ -94,3 +94,83 @@ brew install zopfli
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+
+# --- https://github.com/driesvints/dotfiles/blob/master/Brewfile
+
+# Taps
+tap caskroom/cask
+tap caskroom/fonts
+tap caskroom/versions
+tap homebrew/bundle
+tap homebrew/dupes
+tap homebrew/php
+
+# Install ZSH
+brew zsh
+brew zsh-completions
+
+# Install Binaries
+# brew 'awscli'
+brew git
+brew hub
+brew tree
+# brew 'mackup'
+brew mas
+brew node
+brew trash
+
+# Development
+brew php71, args: ['with-debug']
+brew php71-intl
+brew mysql
+# Todo: Percona?
+
+# Apps
+# cask 'aerial'
+cask 'alfred'
+# cask 'caffeine'
+cask 'dropbox'
+cask 'firefox'
+# cask 'google-backup-and-sync'
+cask 'google-chrome'
+# cask 'gpgtools'
+cask 'iterm2'
+cask 'mysqlworkbench'
+cask 'phpstorm'
+# cask 'quip'
+cask 'sequel-pro'
+# cask 'shuttle'
+cask 'slack'
+# cask 'sublime-text'
+# cask 'torbrowser'
+cask 'tower'
+# cask 'transmit'
+cask 'vagrant'
+cask 'virtualbox'
+cask 'virtualbox-extension-pack'
+cask 'vlc'
+
+# Quicklook
+cask qlcolorcode
+cask qlmarkdown
+cask quicklook-json
+cask quicklook-csv
+cask qlstephen
+
+# Fonts
+cask font-sauce-code-powerline
+cask font-source-code-pro
+cask font-source-sans-pro
+cask font-source-serif-pro
+
+# Install Mac App Store apps
+mas '1Password', id: 443987910
+# mas 'Byword', id: 420212497
+# mas 'Deckset', id: 847496013
+# mas 'Gapplin', id: 768053424
+mas 'Giphy Capture', id: 668208984
+mas 'Paw', id: 584653203
+# mas 'Sip', id: 507257563
+# mas 'Sketch', id: 402476602
+# mas 'Tweetbot', id: 557168941
